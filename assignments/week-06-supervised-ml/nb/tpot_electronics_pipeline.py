42feb81414c2a6bc429a73bbf5532d7aaff2e819
Nkem Michael Onuorah <mikeolysis2380@gmail.com>
2023-01-28 04:01:55 -0500

    Changes Uploading to GitHub Online!

--- assignments/week-06-supervised-ml/nb/tpot_electronics_pipeline.py
+++ assignments/week-06-supervised-ml/nb/tpot_electronics_pipeline.py
@@ -1,9 +1,9 @@
 import numpy as np
 import pandas as pd
 from sklearn.decomposition import FastICA
-from sklearn.ensemble import ExtraTreesClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import make_pipeline
+from sklearn.svm import LinearSVC
 from tpot.export_utils import set_param_recursive
 
 # NOTE: Make sure that the outcome column is labeled 'target' in the data file
@@ -14,8 +14,8 @@ training_features, testing_features, training_target, testing_target = \
 
 # Average CV score on the training set was: 1.0
 exported_pipeline = make_pipeline(
-    FastICA(tol=0.30000000000000004),
-    ExtraTreesClassifier(bootstrap=True, criterion="entropy", max_features=0.9000000000000001, min_samples_leaf=2, min_samples_split=5, n_estimators=100)
+    FastICA(tol=0.75),
+    LinearSVC(C=20.0, dual=False, loss="squared_hinge", penalty="l1", tol=0.1)
 )
 # Fix random state for all the steps in exported pipeline
 set_param_recursive(exported_pipeline.steps, 'random_state', 42)
